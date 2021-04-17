965961889ae68f118791632886f794430ca2a8a8
ayfor <josh.a.stubbington@gmail.com>
2021-04-17 11:05:36 -0400

    installed dependencies & started prompt questions

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    //create data stucture to organize entries 
+    let data = {
+        title:'',
+        description:'',
+        installation:'',
+        usage:'',
+        contribution:'',
+        tests:'',
+        license:'',
+        questions:''
+    }
+
+    inquirer
+        .prompt([
+            {
+            type: 'input',
+            message: 'What is your user name?',
+            name: 'title',
+            },
+            {
+            type: 'input',
+            message: 'Describe your application:',
+            name: 'description',
+            },
+            {
+            type: 'input',
+            message: 'How do you install your application?',
+            name: 'installation',
+            },
+            {
+            type: 'input',
+            message: 'How do you use your application?',
+            name: 'usage',
+            },
+            {
+            type: 'input',
+            message: 'How should developers contribute to your project?',
+            name: 'contribution',
+            },
+            {
+            type: 'input',
+            message: 'What is the testing procedure for your project?',
+            name: 'tests',
+            },
+        ])
+}
 
 // Function call to initialize app
 init();
